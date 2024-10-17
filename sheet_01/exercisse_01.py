e78b643b6130a0123ad95b9ef11d17f02a96f5fd
Johannes Schade <jo.schade@posteo.de>
2024-10-17 10:20:03 +0200

    cosmetics

--- sheet_01/exercisse_01.py
+++ sheet_01/exercisse_01.py
@@ -9,7 +9,7 @@ def explicit_euler(fun, y_0, x_grid):
         h = x_grid[i] - x_grid[i-1]
         y_hat_grid.append(y_hat_grid[i-1] + h * fun(x_grid[i-1], y_hat_grid[i-1]))
 
-    return y_hat_grid
+    return np.array(y_hat_grid)
 
 
 # ODE solved for y'
@@ -22,7 +22,6 @@ h_2 = .2
 # Interval discretization
 I_1 = np.linspace(0, 1, int(1/h_1+1))
 I_2 = np.linspace(0, 1, int(1/h_2+1))
-
 
 # Compute explicit Euler
 y_hat_1 = explicit_euler(ode, 1, I_1)
@@ -39,7 +38,11 @@ plt.plot(I_2, y_hat_2, color='g', marker = 'o', label='h = 0.2')
 plt.legend()
 plt.show()
 
-# Computer errors
+# Output values on grid
+print(f'{y_hat_1=}')
+print(f'{y_hat_2=}')
+
+# Compute errors
 error_1 = abs(y_hat_1[-1] - y_grid[-1])
 error_2 = abs(y_hat_2[-1] - y_grid[-1])
 
