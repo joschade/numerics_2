f5e9e00d2b4b0870f06bc743c1e940f88a60b4a5
Johannes Schade <jo.schade@posteo.de>
2024-10-19 08:34:05 +0200

    utils added

---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.sparse as sp
+
+def reduced_poisson_matrix(n):
+    offdiag = np.ones((n,))
+    diag = 2 * offdiag
+    data = np.array([offdiag, diag, offdiag])
+    offset = np.array([-1, 0, 1])
+
+    return sp.dia_matrix((data, offset), shape=(n, n))
+
+
+def explicit_method(ode: callable, y_0: float, x_grid: np.array, Phi: callable):
+    y_hat_grid = [y_0]
+    for i in range(1, x_grid.size):
+        h = x_grid[i] - x_grid[i - 1]
+        y_hat_grid.append(y_hat_grid[i - 1] + h * Phi(ode, x_grid[i - 1], y_hat_grid[i - 1]))
+
+    return np.array(y_hat_grid)
