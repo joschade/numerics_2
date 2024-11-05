a9082fe2d4fcc27eeb6575687a5a758febded6f7
Johannes Schade <jo.schade@posteo.de>
2024-11-05 15:47:33 +0100

    cosmetics

--- sheet_03_ex03.py
+++ sheet_03_ex03.py
@@ -1,19 +1,18 @@
 import scipy.sparse as sp
 import numpy as np
 import pandas as pd
-from utils import reduced_poisson_matrix, cartesian_product
+from utils import reduced_poisson_matrix, cartesian_product, grid_from_stepsize
 from scipy.sparse.linalg import norm, inv
 from sheet02_ex03 import f
 
 # for multiprocessing
 import concurrent.futures
-import itertools
 
-
-# define list of stepsizes
 
 if __name__ == "__main__":
     print('Sheet 03 Question 03 Subproblem a)')
+
+    # define list of stepsizes
     hs = [2 ** i for i in range(3, 9)]
 
 
@@ -54,15 +53,16 @@ if __name__ == "__main__":
         A = reduced_poisson_matrix(x_grid.size)
         x_0 = np.zeros(x_grid.size)
         _, iter, err = damped_jacobi(A, x_0, f_grid, omega)
-        return h, iter, err
+        return h, omega, iter, err
 
 
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
         results = list(executor.map(damped_jacobi_mp, *cartesian_product(np.array(hs), omegas)))
 
-    df_results = pd.DataFrame(results, columns=['stepsize_inv', 'iterations', 'error'])
-    print(df_results)
+    df_dampedjac = pd.DataFrame(results, columns=['stepsize_inv', 'omega', 'iterations', 'error'])
+    df_dampedjac.to_csv('/sheet_03/df_dampedjac.csv')
+    print(df_dampedjac)
 
 # subproblem c)
 def sor(A: np.array, x_0: np.array, b:np.array, omega: float, minerr=1e-10, maxiter=1e5) -> np.array:
@@ -82,28 +82,25 @@ def sor(A: np.array, x_0: np.array, b:np.array, omega: float, minerr=1e-10, maxi
 
     return x_k, iter-1, err
 
-"""
 if __name__ == "__main__":
     print('Sheet 03 Question 03 Subproblem c)')
 
     epsilons = grid_from_stepsize(.1, 2., .1)
 
 
-    for h in hs:
-        print(f'for {h=}:')
+    def sor_mp(h, omega: float):
         x_grid = np.linspace(0, 1, h)[1:-1]
         f_grid = f(x_grid)
 
         A = reduced_poisson_matrix(x_grid.size)
         x_0 = np.zeros(x_grid.size)
-        relax = []
-        for epsilon in epsilons:
-            print(f'  for {epsilon=}:')
+        _, iter, err = sor(A, x_0, f_grid, omega)
+        return h, omega, iter, err
 
-            x, iter, err = sor(A, x_0, f_grid, epsilon)
-            print(f'    iterations: {iter}, error: {err}')
-            relax.append(iter)
 
-        argmax = np.argmin(np.array(relax))
-        print(f'  best relaxation factor: {epsilons[argmax]}')
-"""
+        with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
+            results = list(executor.map(sor_mp, *cartesian_product(np.array(hs), omegas)))
+
+        df_sor = pd.DataFrame(results, columns=['stepsize_inv', 'omega', 'iterations', 'error'])
+        df_sor.to_csv('/sheet_03/df_sor.csv')
+        print(df_dampedjac)
