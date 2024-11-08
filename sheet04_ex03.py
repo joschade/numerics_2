890b36a13f2007a7fd51a0b97d127359a8f5752d
Johannes Schade <jo.schade@posteo.de>
2024-11-08 09:11:45 +0100

    include relaxation

--- sheet04_ex03.py
+++ sheet04_ex03.py
@@ -5,7 +5,7 @@ from sheet02_ex03 import f
 from sheet03_ex03 import sor
 from concurrent.futures import ProcessPoolExecutor
 
-optimal_relax = lambda h: 1/(1+np.sin(h*np.pi))
+optimal_relax = lambda h: 2/(1+np.sin(h*np.pi))
 
 
 if __name__ == "__main__":
@@ -21,11 +21,11 @@ if __name__ == "__main__":
         A = reduced_poisson_matrix(x_grid.size)
         x_0 = np.zeros(x_grid.size)
         _, iter, err = sor(A, x_0, f_grid, optimal_relax(1 / h))
-        return h, iter, err
+        return h, optimal_relax(1 / h), iter, err
 
     with ProcessPoolExecutor(max_workers=len(hs)) as executor:
         results = list(executor.map(sor_mp, hs))
 
-    df_sor = pd.DataFrame(results, columns=['stepsize_inv', 'iterations', 'error'])
+    df_sor = pd.DataFrame(results, columns=['stepsize_inv', 'omega', 'iterations', 'error'])
     df_sor.to_csv('sheet_04/df_sor.csv')
-    print(df_sor)k
+    print(df_sor)
